4c5a377b8dfc7624c1dc2a3bf34dafebdef0934c
LizavetaLiakh <lizaveta.liakh@innowise.com>
2025-12-08 22:35:11 +0300

    Add JavaDocs

--- src/main/java/com/innowise/payment/entity/Payment.java
+++ src/main/java/com/innowise/payment/entity/Payment.java
@@ -8,6 +8,9 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+/**
+ * Entity that stores information about payments.
+ */
 @Entity
 @Table(name = "payments")
 @Data
@@ -15,24 +18,42 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class Payment {
 
+    /**
+     * Unique identifier of the payment. Generates automatically by the database.
+     */
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Unique identifier of the order from table "orders" in orders' database.
+     */
     @Column(name = "order_id", nullable = false)
     private Long orderId;
 
+    /**
+     * Unique identifier of the user from table "users" in users' database.
+     */
     @Column(name = "user_id", nullable = false)
     private Long userId;
 
+    /**
+     * The status of the payment.
+     */
     @Enumerated(EnumType.STRING)
     @Column(name = "status", nullable = false)
     private PaymentStatus status;
 
+    /**
+     * Timestamp when the payment was made.
+     */
     @Column(name = "time_stamp", nullable = false)
     private LocalDateTime timestamp;
 
+    /**
+     * The total price of the order.
+     */
     @Column(name = "payment_amount", nullable = false)
     private BigDecimal paymentAmount;
 }
